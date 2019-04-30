9ec7360e2927025b3eff4cc155e5b0ca8f16f8ad
Stephen Williamson <mycomputer@Stephens-MacBook.local>
2019-04-30 14:05:12 -0400

    Added 3rd party shit

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
   })
   export class AuthService {
     baseURL = 'http://localhost:5000/api/auth/';
+    jwtHelper = new JwtHelperService();
+    decodedToken: any;
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +19,7 @@ import { map } from 'rxjs/operators';
           const user = response;
           if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
     );
@@ -23,5 +27,10 @@ import { map } from 'rxjs/operators';
 
   register(model: any) {
     return this.http.post(this.baseURL + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
